04cfd4a78b454615ed788bf5efb4904144fe0a39
Jose Luis Cabrera <cabrera@inkuba.com>
2022-11-03 11:32:43 -0300

    fix: return only airport with flights

--- server/api/airports.get.ts
+++ server/api/airports.get.ts
@@ -1,5 +1,10 @@
 import airports from "@/data/iata.json";
+import flights from "@/data/dataset.json";
 import type Airport from "@/types/airport";
+
 export default defineEventHandler((event): Airport[] => {
-  return airports.sort((a, b) => a.PROVINCIA - b.PROVINCIA);
+  // retornamos solo los aeropuertos que tienen vuelos
+  return airports.filter((airport: Airport) => {
+    return flights.some((flight) => flight.origin === airport.IATA);
+  });
 });
