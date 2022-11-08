88c053320aa0641913a7773bba60e4b798187857
Jose Luis Cabrera <cabrera@inkuba.com>
2022-11-08 12:01:22 -0300

    feat: title app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      title: "FlyBondi Challenge",
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@vueuse/nuxt",
